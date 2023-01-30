6b61ceec0d684f6cc8991e1d64bc9cff50365d4f
Cluley <oo43@yandex.ru>
2023-01-30 14:41:36 +0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+class Counter {
+  public:
+      Counter(int input = 1) { var = input; }
+      void increase() { ++var; }
+      void decrease() { --var; }
+      void print();
+  private:
+      int var;
+};
+
+void Counter::print() {
+    std::cout << var << std::endl;
+}
+
+int main(int argc, char** argv){
+    std::string answer;
+    
+    while (true) {
+        std::cout << "Do you want to set an initital value of the counter? Enter yes/no: ";
+        std::cin >> answer;
+        if (answer == "yes") {
+            break;
+        }
+        else if (answer == "no") {
+            break;
+        }
+    }
+    
+        if (answer == "yes") {
+            int input;
+            std::cout << "Enter the initial value of the counter: ";
+            std::cin >> input;
+            Counter var(input);
+            char optn;
+            do {
+                std::cout << "Enter operation ('+', '-', '=' or 'x'): ";
+                std::cin >> optn;
+                if (optn == '+') {
+                    var.increase();
+                }
+                else if (optn == '-') {
+                    var.decrease();
+                }
+                else if (optn == '=') {
+                    var.print();
+                }
+            } while (optn != 'x');
+        }
+        else if (answer == "no") {
+            Counter var;
+            char optn;
+            do {
+                std::cout << "Enter operation ('+', '-', '=' or 'x'): ";
+                std::cin >> optn;
+                if (optn == '+') {
+                    var.increase();
+                }
+                else if (optn == '-') {
+                    var.decrease();
+                }
+                else if (optn == '=') {
+                    var.print();
+                }
+            } while (optn != 'x');
+        }
+
+    std::cout << "Good bye!" << std::endl;
+
+    return 0;
+}
